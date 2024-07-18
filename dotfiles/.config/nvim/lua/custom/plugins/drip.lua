3c77501bc69587ad26373777b2cfdf3cca9dd383
James Xiao <xtremejames1@gmail.com>
2024-07-18 10:24:57 -0500

    fixed vim theme

--- dotfiles/.config/nvim/lua/custom/plugins/drip.lua
+++ dotfiles/.config/nvim/lua/custom/plugins/drip.lua
@@ -1,79 +1,79 @@
 return {
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   priority = 1000,
+  -- },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    'rebelot/kanagawa.nvim',
     priority = 1000,
     config = function()
-      vim.cmd('colorscheme catppuccin-mocha')
+      vim.cmd 'colorscheme kanagawa'
     end,
   },
   {
-    "rebelot/kanagawa",
-
-  }
-  {
     'akinsho/bufferline.nvim',
-    version = "*",
-    dependencies = 'nvim-tree/nvim-web-devicons'
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
   },
   {
-    "karb94/neoscroll.nvim",
+    'karb94/neoscroll.nvim',
     config = function()
-      require("neoscroll").setup({})
+      require('neoscroll').setup {}
     end,
   },
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
   {
-    "folke/noice.nvim",
-    event = "VeryLazy",
+    'folke/noice.nvim',
+    event = 'VeryLazy',
     opts = {
       -- add any options here
     },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
+      'MunifTanjim/nui.nvim',
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    }
+      'rcarriga/nvim-notify',
+    },
   },
   {
     'stevearc/dressing.nvim',
     opts = {},
   },
   {
-    "RRethy/vim-illuminate",
+    'RRethy/vim-illuminate',
     opts = {},
     config = function()
       require('illuminate').configure()
-    end
+    end,
   },
   {
-    "echasnovski/mini.indentscope",
+    'echasnovski/mini.indentscope',
     opts = {
       -- symbol = "▏",
-      symbol = "│",
+      symbol = '│',
       options = { try_as_border = true },
     },
     init = function()
-      vim.api.nvim_create_autocmd("FileType", {
+      vim.api.nvim_create_autocmd('FileType', {
         pattern = {
-          "help",
-          "alpha",
-          "Alpha",
-          "dashboard",
-          "neo-tree",
-          "Trouble",
-          "trouble",
-          "lazy",
-          "mason",
-          "notify",
-          "toggleterm",
-          "lazyterm",
+          'help',
+          'alpha',
+          'Alpha',
+          'dashboard',
+          'neo-tree',
+          'Trouble',
+          'trouble',
+          'lazy',
+          'mason',
+          'notify',
+          'toggleterm',
+          'lazyterm',
         },
         callback = function()
           vim.b.miniindentscope_disable = true
